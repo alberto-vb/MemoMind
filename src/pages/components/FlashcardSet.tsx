6fee35c8385501bacda62075cbd7badaa1a515e3
alberto-vb <vegara.belmonte.alberto@gmail.com>
2024-06-25 20:17:36 +0200

    Work in progress that was left behind

--- src/pages/components/FlashcardSet.tsx
+++ src/pages/components/FlashcardSet.tsx
@@ -1,35 +1,35 @@
-import { api } from '~/utils/api'
+import { api } from "~/utils/api";
 
 interface Card {
-  question: string
-  answer: string
+    question: string;
+    answer: string;
 }
 
 interface Set {
-  name: string
-  cards: Card[]
+    name: string;
+    cards: Card[];
 }
 
 export default function FlashingCardSets() {
-  const sets = api.test.listFlashcardsSets.useQuery().data
-  console.log('Sets', sets)
-  return (
-    <>
-      <div>
-        {sets &&
-          sets.map((set: Set) => (
-              <div className='text-white' key={set.name}>
-                {set.name}
-                {set.cards &&
-                  set.cards.map((card: Card, index: number) => (
-                    <div key={index}>
-                      <div>{card.question}</div>
-                      <div>{card.answer}</div>
-                    </div>
-                  ))}
-              </div>
-          ))}
-      </div>
-    </>
-  )
+    const sets = api.test.listFlashcardsSets.useQuery().data;
+    console.log("Sets", sets);
+    return (
+        <>
+            <div>
+                {sets &&
+                    sets.map((set: Set) => (
+                        <div className="text-white" key={set.name}>
+                            {set.name}
+                            {set.cards &&
+                                set.cards.map((card: Card, index: number) => (
+                                    <div key={index}>
+                                        <div>{card.question}</div>
+                                        <div>{card.answer}</div>
+                                    </div>
+                                ))}
+                        </div>
+                    ))}
+            </div>
+        </>
+    );
 }
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,85 +1,86 @@
 import { z } from "zod";
+import fs from "fs/promises";
 
 import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
+    createTRPCRouter,
+    protectedProcedure,
+    publicProcedure,
 } from "~/server/api/trpc";
 
 interface Card {
-  question: string;
-  answer: string;
+    question: string;
+    answer: string;
 }
 
 interface Set {
-  name: string;
-  cards: Card[];
+    name: string;
+    cards: Card[];
 }
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+    hello: publicProcedure
+        .input(z.object({ text: z.string() }))
+        .query(({ input }) => {
+            return {
+                greeting: `Hello ${input.text}`,
+            };
+        }),
+
+    getAll: publicProcedure.query(({ ctx }) => {
+        return ctx.prisma.example.findMany();
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+    getSecretMessage: protectedProcedure.query(() => {
+        return "you can now see this secret message!";
+    }),
 });
 
 export const listFlashcards = createTRPCRouter({
-  list: publicProcedure.query(async () => {
-    const fs = require("fs/promises");
-    const data = await fs.readFile("result.json", "utf-8");
-    const cards: Card[] = JSON.parse(data.toString());
-    return cards;
-  }),
-  listFlashcardsSets: publicProcedure.query(async () => {
-    const fs = require("fs/promises");
-    const data = await fs.readFile("flashcardssets.json", "utf-8");
-    const cards: Set[] = JSON.parse(data.toString());
-    return cards;
-  }),
+    list: publicProcedure.query(async () => {
+        const data = await fs.readFile("result.json", "utf-8");
+        const cards: Card[] = JSON.parse(data.toString()) as Card[];
+        return cards;
+    }),
+    listFlashcardsSets: publicProcedure.query(async () => {
+        const data = await fs.readFile("flashcardssets.json", "utf-8");
+        const cards: Set[] = JSON.parse(data.toString()) as Set[];
+        return cards;
+    }),
 });
 
 export const mutationRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(
-      z.object({
-        question: z.string(),
-        answer: z.string(),
-      }),
-    )
-    .mutation(async ({ input }) => {
-      const fs = require("fs/promises");
-      const card = {
-        question: input.question,
-        answer: input.answer,
-      };
-      try {
-        // Read the existing data from the file
-        const data = await fs.readFile("result.json", "utf-8");
+    hello: publicProcedure
+        .input(
+            z.object({
+                question: z.string(),
+                answer: z.string(),
+            }),
+        )
+        .mutation(async ({ input }) => {
+            const card = {
+                question: input.question,
+                answer: input.answer,
+            };
+            try {
+                // Read the existing data from the file
+                const data = await fs.readFile("result.json", "utf-8");
 
-        // Parse the JSON data into a JavaScript object
-        const cards = JSON.parse(data);
+                // Parse the JSON data into a JavaScript object
+                const cards = JSON.parse(data) as Card[];
 
-        // Add a new object to the array
-        cards.unshift(card);
+                // Add a new object to the array
+                cards.unshift(card);
 
-        // Write the updated JSON data back to the file
-        await fs.writeFile("result.json", JSON.stringify(cards, null, 2));
+                // Write the updated JSON data back to the file
+                await fs.writeFile(
+                    "result.json",
+                    JSON.stringify(cards, null, 2),
+                );
 
-        return { card };
-      } catch (error) {
-        console.error(error);
-        throw new Error("Failed to add card to file");
-      }
-    }),
+                return { card };
+            } catch (error) {
+                console.error(error);
+                throw new Error("Failed to add card to file");
+            }
+        }),
 });
